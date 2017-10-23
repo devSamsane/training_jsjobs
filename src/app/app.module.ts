7131a08ba66414f1a252f675da7c3bd02cbe6621
Olivier <devangularjs@gmail.com>
2017-10-23 22:49:21 +0200

    section(1)(2|3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { JobListComponent } from './job-list/job-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule
